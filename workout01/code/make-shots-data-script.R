91805ccb7c3dd74670560a1e466567941f6ba2d4
Sooyoung Shin <bhyyshin@berkeley.edu>
2019-03-12 22:39:33 -0700

    code: add shot data rscript file

---
+++ workout01/code/make-shots-data-script.R
@@ -0,0 +1,102 @@
+####################
+#title: Make-Shots-Data-Script.R
+#description: This script is deal with data preparation for create a csv data ???le shots-data.csv. 
+#inputs: andre-iguodala.csv, draymond-green.csv, kevin-durant.csv, klay-thompson.csv, stephen-curry.csv
+#outputs: 
+
+library(dplyr)
+library(plyr)
+
+getwd()
+setwd('C:/Users/jsbshin/Desktop/hw-stat133/workout01/code')
+
+
+colClass = c("character", "character", "integer", "integer", 
+             "integer", "integer", "factor", "factor", "factor", "integer", 
+             "character", "integer", "integer" )
+
+curry <- read.csv("../data/stephen-curry.csv", stringsAsFactors = FALSE, colClasses = colClass )
+
+durant <- read.csv("../data/kevin-durant.csv", stringsAsFactors = FALSE, colClasses = colClass )
+
+iguodala <- read.csv("../data/andre-iguodala.csv", stringsAsFactors = FALSE, colClasses = colClass)
+
+green <- read.csv("../data/draymond-green.csv", stringsAsFactors = FALSE, colClasses = colClass)
+
+thompson <- read.csv("../data/klay-thompson.csv", stringsAsFactors = FALSE, colClasses = colClass)
+
+
+
+
+curry$name <- "Stephen Curry"
+
+durant$name <- "Kevin Durant"
+
+iguodala$name <- "Andre Iguodala"
+
+green$name <- "Draymond Green"
+
+thompson$name <- "Klay Thompson"
+
+
+
+
+curry$shot_made_flag <- revalue(curry$shot_made_flag, 
+                                c( "n" = "shot_no", "y" = "shot_yes"))
+
+durant$shot_made_flag <- revalue(durant$shot_made_flag, 
+                                c( "n" = "shot_no", "y" = "shot_yes"))
+
+iguodala$shot_made_flag <- revalue(iguodala$shot_made_flag, 
+                                c( "n" = "shot_no", "y" = "shot_yes"))
+
+green$shot_made_flag <- revalue(green$shot_made_flag, 
+                                c( "n" = "shot_no", "y" = "shot_yes"))
+
+thompson$shot_made_flag <- revalue(thompson$shot_made_flag, 
+                                c( "n" = "shot_no", "y" = "shot_yes"))
+
+
+
+curry$minute <- ((12 * curry$period) - (curry$minutes_remaining))
+
+durant$minute <- ((12 * durant$period) - (durant$minutes_remaining))
+
+iguodala$minute <- ((12 * iguodala$period) - (iguodala$minutes_remaining))
+
+green$minute <- ((12 * green$period) - (green$minutes_remaining))
+
+thompson$minute <- ((12 * thompson$period) - (thompson$minutes_remaining))
+
+
+
+  
+sink(file = "../output/stephen-curry-summary.txt")
+summary(curry)
+sink()
+
+sink(file = "../output/kevin-durnat-summary.txt")
+summary(durant)
+sink()
+
+sink(file = "../output/andre-iguodala-summary.txt")
+summary(iguodala)
+sink()
+
+sink(file = "../output/draymond-green-summary.txt")
+summary(green)
+sink()
+
+sink(file = "../output/klay-thompson-summary.txt")
+summary(thompson)
+sink()
+
+
+shots_data <- rbind(curry, iguodala, green, thompson, durant)
+write.csv(shots_data, "../data/shots-data.csv")
+
+
+sink(file = "../output/shots-data-summary.txt")
+summary(shots_data)
+sink()
+
